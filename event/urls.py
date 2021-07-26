3a70dd7286c97d9dd377eafced33254718ff6dcb
bakdaulet <bakosh21345@gmail.com>
2021-07-26 14:10:05 +0600

    a

--- event/urls.py
+++ event/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('events/', views.eventView, name='events'),
+    path('today-view/', views.todayView, name='todayView')
 ]
